74b21e93740b978a60e119c6027632f6de7fd10a
kaybee019 <kaybee@blklabs.com>
2024-12-29 00:08:49 +0100

    Added pump.fun url

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const socials = [
   { name: "twitter", link: "https://x.com/pmpcoin" },
-  { name: "pump.fun", link: "https://pump.fun/coin/" },
+  { name: "pump.fun", link: "https://pump.fun/coin/A8SPRUzguJZ6zZbsKq8YSxSTTjasZvbmVTSj6Kn8pump/" },
   { name: "telegram", link: "https://telegram.org/" },
 ];
